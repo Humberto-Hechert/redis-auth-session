cb7e278ef1573df0de5c9dd437a93d7d76478125
humberto.luis <humberto.luis@pontaltech.com.br>
2025-02-20 21:18:47 -0300

    feat: create server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import router from './router';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+export default app;
